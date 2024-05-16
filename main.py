9cb4ec4dcf23b683292072915487985208c58362
mayimounadama <mayimounadama@gmail.com>
2024-05-16 11:51:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import streamlit as st
 import json
 
 # Set the title of the Streamlit app
-st.title('JSON File Reader')
+with st.sidebar:
 
-# Upload a JSON file
-uploaded_file = st.file_uploader("Choose a JSON file", type="json")
+    st.title('JSON File Reader')
+    # Upload a JSON file
+    uploaded_file = st.file_uploader("Choose a JSON file", type="json")
 
-if uploaded_file is not None:
-    # Read the JSON file
-    data = json.load(uploaded_file)
+    if uploaded_file is not None:
+        # Read the JSON file
+        data = json.load(uploaded_file)
 
-    # Display the JSON data
-    st.json(data)
+        # Display the JSON data
+        st.json(data)
